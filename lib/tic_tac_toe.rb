a220fee98015b4c380123bcf4fe9b2ba4d726157
Clark Chen <yyunqingchenn@gmail.com>
2019-09-02 03:31:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,9 +79,9 @@ def won?
     index_1 = win_combo[1]
     index_2 = win_combo[2]
 
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
