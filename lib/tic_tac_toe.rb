b87f5e318fa3d7c5c572a8a2d212391d5eda96ca
Clark Chen <yyunqingchenn@gmail.com>
2019-09-02 03:23:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,16 @@
 class TicTacToe 
-  def initialize()
+  def initialize(board)
+    @board = board || Array.new(9, " ")
+  end 
+  
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end 
+  
+  def turn_count 
+    @board.count{|token| token == "X" || token == "O"}
+  end 
+  
+  def display_board
+    
 end 
